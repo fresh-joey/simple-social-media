ea1616caddaa8bebfed657a1b4f3aabb15136357
fresh-joey <johserf30@gmail.com>
2024-03-20 00:38:36 +0100

    update deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -12,32 +12,6 @@ echo "Deployment started ...."
 git pull origin main
 
 
-# Clear the old cache
-# php artisan clear-compiled
-
-# Recreate cache
-# php artisan optimize
-
-# Install Nodejs
-curl -sL https://deb.nodesource.com/setup_21.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install nodejs
-node -v
-rm nodesource_setup.sh
-
-# Compile npm assets
-npm install
-npm run dev
-npm run prod
-
-# Run database migrations
-php artisan migrate --force
-
-sudo chown -R $USER:www-data /var/www/social_media/storage
-sudo chown -R $USER:www-data /var/www/social_media/bootstrap/cache
-sudo chmod -R 775 /var/www/social_media/storage
-sudo chmod -R 755 /var/www/social_media/bootstrap/cache
-
 # Exit maintenance mode
 php artisan up
 
