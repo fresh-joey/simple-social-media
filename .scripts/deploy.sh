f13aae86687fe3deb5cc5ba7644f29cccc96eafb
fresh-joey <johserf30@gmail.com>
2024-03-19 20:58:07 +0100

    update deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,7 +5,7 @@ echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-# (php artisan down) || true
+(php artisan down) || true
 
 # Pull the latest version of the app
 git pull origin main
