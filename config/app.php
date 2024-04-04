9b15be99f6e1a1745b0d368d2e06d148afdd5280
fresh-joey <johserf30@gmail.com>
2024-04-04 15:33:39 +0100

    Changed the title

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Social Media App'),
+    'name' => env('APP_NAME', 'Social_Media_App'),
 
     /*
     |--------------------------------------------------------------------------
