611fb83310ec60bdd02802eef9f2fca778e29ae4
fresh-joey <johserf30@gmail.com>
2024-04-04 11:43:05 +0100

    edited login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,12 +35,12 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Did you Forget your password?') }}
+                        {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
                 <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
+                    {{ __('Log in Now!') }}
                 </x-jet-button>
             </div>
         </form>
