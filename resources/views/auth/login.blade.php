eace46ceb69c4b56a0e59d32a05a5b0e55cfdcdd
fresh-joey <johserf30@gmail.com>
2024-04-05 12:01:00 +0100

    Changed the title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,14 +21,14 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Enter Your Password') }}" />
+                <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Please, Remember me') }}</span>
                 </label>
             </div>
 
